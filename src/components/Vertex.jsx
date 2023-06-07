8856cbe30e384f3455bdbd932047deafc1bb4b3d
avatar-sd <sourabh.abhiwan02@gmail.com>
2023-06-07 07:59:50 +0530

    one algo

--- src/components/Vertex.jsx
+++ src/components/Vertex.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Vertex({ data, EdgeCreateEnd, EdgeCreateStart,deleteVertex,stack }) {
     const { x, y, value,index } = data;
     const {pastStack, fetureStack, currentStatus}=stack?stack:{};
-    let current_vertex=fetureStack?.[0]
+    let current_vertex=currentStatus
 
     return (
         <div onDoubleClick={(e)=>{
            
             deleteVertex?.({index})
-            }} className={`z-50 cursor-pointer flex items-center justify-center h-8 w-8 rounded-full border-2 ${current_vertex?.visited?.[current_vertex?.visited?.length-1]==index?"border-red-500":   current_vertex?.visited?.indexOf(`${index}`)!=-1?"border-blue-500":   "border-gray-400"}  bg-white text-gray-400 hover:text-purple-400 hover:border-purple-400 absolute -translate-x-1/2 -translate-y-1/2`} style={{
+            }} className={`z-50 cursor-pointer flex items-center justify-center h-8 w-8 rounded-full border-2 ${current_vertex?.visited?.[current_vertex?.visited?.length-1]==index?"border-red-500":current_vertex?.visited==undefined?"border-gray-400":   current_vertex?.visited?.indexOf(`${index}`)==-1?"border-gray-400":"border-blue-500"   }  bg-white text-gray-400 hover:text-purple-400 hover:border-purple-400 absolute -translate-x-1/2 -translate-y-1/2`} style={{
             // }} className='z-50 cursor-pointer flex items-center justify-center h-8 w-8 rounded-full border border-gray-400 bg-blue-500 animate-ping text-gray-400 hover:text-purple-400 hover:border-purple-400 absolute -translate-x-1/2 -translate-y-1/2' style={{
             top: `${y}px`,
             left: `${x}px`
